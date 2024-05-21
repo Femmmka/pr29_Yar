327d61383281161b599ff244558eeb83d639bd20
Ефим <fimakrut@mail.ru>
2024-05-21 12:33:26 +0500

    Создание класса подключения

---
+++ pr29_Yar/Classes/Common/Config.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr29_Yar.Classes.Common
+{
+    public class Config
+    {
+        public static string ConnectionConfig = "server=127.0.0.1;port=3306;uid=root;pwd=;database=films;SslMode=None;";
+
+        public static MySqlServerVersion Version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
