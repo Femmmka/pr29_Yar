7bbd78f12de301767c8576b64a67ec2cfad32dfc
Ефим <fimakrut@mail.ru>
2024-05-21 12:31:31 +0500

    Добавьте файлы проекта.

---
+++ pr29_Yar/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace pr29_Yar
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
