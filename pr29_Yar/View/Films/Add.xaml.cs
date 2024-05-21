06bfa7eca647f4724f97a32547e169a9174454ae
Ефим <fimakrut@mail.ru>
2024-05-21 13:34:11 +0500

    Создание файлов в папке View

---
+++ pr29_Yar/View/Films/Add.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+
+namespace pr29_Yar.View.Films
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Page
+    {
+        public Add(object? Context)
+        {
+            InitializeComponent();
+            DataContext = Context;
+        }
+    }
+}
